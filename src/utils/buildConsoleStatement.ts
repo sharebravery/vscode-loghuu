c50b223f3d7d01b5bb61114905ed5df7eef6ec4b
sharebravery <sharebravery@gmail.com>
2024-01-16 14:02:23 +0800

    feat: templateString

--- src/utils/buildConsoleStatement.ts
+++ src/utils/buildConsoleStatement.ts
@@ -1,10 +1,9 @@
-/* eslint-disable no-template-curly-in-string */
 // utils/buildConsoleStatement.ts
 import * as vscode from 'vscode'
 import { getRandomColor } from './getRandomColor'
 
 export function buildConsoleStatement(selectedText: string, currentLineNumber: number, commandType: string): string {
-  const config = vscode.workspace.getConfiguration('Loghuu')
+  const config = vscode.workspace.getConfiguration()
 
   const randomColor = getRandomColor()
   const prefix = config.get('prefix') as string
@@ -12,12 +11,12 @@ export function buildConsoleStatement(selectedText: string, currentLineNumber: n
 
   if (selectedText && (selectedText.toLowerCase() === 'error' || selectedText.toLowerCase() === 'err')) {
     return templateString
-      ? templateString.replace('${selectedText}', selectedText).replace('${currentLineNumber}', currentLineNumber.toString())
+      ? templateString.replace(/\${prefix}/g, prefix).replace(/\${selectedText}/g, selectedText).replace(/\${currentLineNumber}/g, currentLineNumber.toString()).replace(/\${randomColor}/g, randomColor)
       : `console.error('%c[${selectedText}]-${currentLineNumber}:', 'color: ${randomColor}', ${selectedText});\n`
   }
   else {
     return templateString
-      ? templateString.replace('${selectedText}', selectedText).replace('${currentLineNumber}', currentLineNumber.toString())
+      ? templateString.replace(/\${prefix}/g, prefix).replace(/\${selectedText}/g, selectedText).replace(/\${currentLineNumber}/g, currentLineNumber.toString()).replace(/\${randomColor}/g, randomColor)
       : selectedText
         ? `console.${commandType.toLowerCase()}('%c${prefix}[${selectedText}]-${currentLineNumber}:', 'color: ${randomColor}', ${selectedText});\n`
         : `console.${commandType.toLowerCase()}(' %c${prefix}[]-${currentLineNumber}:', 'color: ${randomColor}',);`
